58f38b8b840c1c624496dd80796ad95bc10bf5b9
sohaib-cyborg <k200488@nu.edu.pk>
2023-12-03 01:39:35 +0500

    changes updated

---
+++ webApit.tests/Controllers/addresscontroller.cs
@@ -0,0 +1,70 @@
+﻿using BusinessAccessLayer.Services.Addresses;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPITask.Controllers;
+using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+
+namespace webApit.tests.Controllers
+{
+    public class addresscontroller
+    {
+        private IAddressServices _addressServices;
+        private AddressController _addresscontroller;
+        public addresscontroller() 
+        {
+            _addressServices = A.Fake<IAddressServices>();
+            _addresscontroller = new AddressController(_addressServices);
+        }
+        [Fact]
+        public void AddressController_test_index()
+        {
+            string user_id = "123";
+            var fakeAddresses = A.Fake<List<AddressViewModel>>(); // Assuming AddressViewModel is your view model
+            A.CallTo(() => _addressServices.GetAddressById(user_id)).Returns(Task.FromResult(fakeAddresses));
+            var result = _addresscontroller.Index(user_id);
+
+            // Assert
+            result.Should().BeOfType < Task<IActionResult>>();
+
+
+        }
+        [Fact]
+        public void AddressController_Index_ReturnsNotFound()
+        {
+            // Arrange
+            string userId = "456";
+            A.CallTo(() => _addressServices.GetAddressById(userId)).Returns(Task.FromResult<List<AddressViewModel>>(null));
+
+            
+            var result = _addresscontroller.Index(userId);
+
+            
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+        [Fact]
+        public void Address_Controller_GetAddress_ReturnsOk()
+        {
+            
+            Guid addressId = Guid.NewGuid();
+            var fakeAddress = A.Fake<AddressViewModel>();
+
+            A.CallTo(() => _addressServices.GetAddress(addressId)).Returns(Task.FromResult(fakeAddress));
+
+          
+            var result = _addresscontroller.GetAddress(addressId).Result;
+
+            
+            result.Should().BeOfType<OkObjectResult>()
+                .Which.Value.Should().BeEquivalentTo(fakeAddress);
+        }
+
+    }
+
+}
+
---
+++ webApit.tests/Controllers/customer.cs
@@ -0,0 +1,53 @@
+﻿using BusinessAccessLayer.Services.Auth;
+using DataAccessLayer.Models;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPITask.Controllers;
+
+namespace webApit.tests.Controllers
+{
+    public class customer
+    {
+        private IAuthServices _authServices;
+        private CustomerController _customerController;
+        public customer() 
+        {
+            _authServices = A.Fake<IAuthServices>();
+            _customerController = new CustomerController(_authServices);
+        }
+        [Fact]
+        public void Customer_controller_index_returnSuccess()
+        {
+            var fakeCustomers = A.Fake<List<CustomerViewModel>>();
+            A.CallTo(() => _authServices.GetCustomers()).Returns(Task.FromResult(fakeCustomers));
+            var result = _customerController.index();
+            result.Should().BeOfType<Task<IActionResult>>();
+
+
+
+        }
+        [Fact]
+        public void Customer_Controller_GetCustomer_Success()
+        {
+            // Arrange
+            string User_id = "175";
+            var fakeCustomer = A.Fake<CustomerViewModel>();
+
+            A.CallTo(() => _authServices.GetCustomerById(User_id)).Returns(Task.FromResult(fakeCustomer));
+
+            // Act
+            var result = _customerController.GetCustomer(User_id).Result;
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>()
+                .Which.Value.Should().BeEquivalentTo(fakeCustomer);
+        }
+      
+    }
+}
